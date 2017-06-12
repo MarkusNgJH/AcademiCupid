9b4b19b5664b999ae9e8fa2c7bfabdf432c6e906
visual-reality <chenyuan-95@hotmail.com>
2017-06-13 00:08:17 +0800

    Add "enrolled" and "projects" attributes to users collection. Add users selection to "eventInvitation" page, but still have bug (the styling of the search-menu is incorrect. This is only solved by refreshing the page).

--- AcademiCupid/client/EventInvitation.js
+++ AcademiCupid/client/EventInvitation.js
@@ -1,13 +1,55 @@
+Template.EventInvitation.onCreated(function() {
+	var currentEvent = Session.get('curentEvent');
+	// document.location.reload(true); //refreshes the page
+});
+
 Template.EventInvitation.events ({
 	'submit form': function(event){
-		event.preventDefault();	
-		var participantName = event.target.participantUsername.value;
-		var currentEvent = Session.get('currentEvent');
-		var user_id = User.findOne({name: participantName})._id;
-		var acct_id = User.findOne({name: participantName}).owner;
-		console.log("user_id " + user_id);
-		console.log("acct_id " + acct_id)
-		User.update(user_id, {$addToSet: {enrolled : currentEvent}});
-		Events.update(currentEvent, {$addToSet: {participants: acct_id}});
+		event.preventDefault();
+		var selectedUsers = document.getElementsByClassName("item active filtered");
+		for(var i = 0; i < selectedUsers.length; i++) {
+			var nextUserId = selectedUsers[i].getAttribute("data-value");
+			Meteor.users.update(nextUserId, {$addToSet: {enrolled: currentEvent}})
+			Events.update(currentEvent, {$addToSet: {participants: nextUserId}});
+		}
 	}
 });
+
+Template.EventInvitation.helpers ({
+	'getUsers': function() {
+		return Meteor.users.find({});
+	}
+});
+
+// var selectedSkills = document.getElementsByClassName("item active filtered");
+// 		var newSkillArr = [];
+// 		var original = Meteor.users.findOne(Meteor.userId());
+// 		if (original.profile.skills === null) {
+// 			var originalSkills = [];
+// 		} else {
+// 			var originalSkills = original.profile.skills;
+// 		}
+// 		for(var i = 0; i < selectedSkills.length; i++) {
+// 			var nextSkill = selectedSkills[i].innerHTML;
+// 			var testSkill = new userSkills(nextSkill, original._id);
+// 			// if (!originalSkills.includes(nextSkill)) {
+// 			newSkillArr.push(testSkill);
+// 			// }
+// 		}
+// 		console.log(newSkillArr);
+// 		// console.log(original.emails.address);
+// 		// console.log(original);
+// 		var fName = event.target.fName.value;
+// 		var lName = event.target.lName.value;
+// 		var updatedSkills = originalSkills.concat(newSkillArr)
+// 		Meteor.users.update(
+// 			Meteor.userId(), { $set: {
+// 								 'profile': {
+// 								 	'firstName': fName,
+// 								 	'lastName': lName,
+// 								 	'skills': updatedSkills
+// 									 }
+
+// 							 	}
+// 							}		
+// 		);
