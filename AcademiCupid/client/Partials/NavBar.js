3f2ae201fa69e4e0f91feb821ba13cb25738f25e
visual-reality <chenyuan-95@hotmail.com>
2017-06-25 15:42:21 +0800

    Format Header. Side Navigation bar will stick to screen as users scroll down the page.

--- AcademiCupid/client/Partials/NavBar.js
+++ AcademiCupid/client/Partials/NavBar.js
@@ -1,6 +1,25 @@
 var parts = location.href.split('/')
 var id = parts.pop()
 
+Template.NavBar.rendered = function () {
+	$(document).ready(function(){
+	    $(window).scroll(function(){
+	        if($(window).scrollTop() > 185) {
+        		if(!$("#NavMenu").hasClass("fixed")) {
+        			$("#NavMenu").removeClass("fluid");
+        			$("#NavMenu").addClass("fixed");
+        		}
+	        }
+	        else if ($(window).scrollTop() < 185){
+	        	if($("#NavMenu").hasClass("fixed")) {
+	        		$("#NavMenu").removeClass("fixed");	
+	        		$("#NavMenu").addClass("fluid");
+	        	}
+	        }
+	    });
+	});
+}
+
 Template.NavBar.events({
 	'click .create-new-project': () => {
 		var parts = location.href.split('/');
@@ -17,5 +36,8 @@ Template.NavBar.helpers({
 	},
 	eventId: function () {
 		return FlowRouter.getParam('eventId');
+	},
+	currentUserId: function() {
+		return Meteor.userId();
 	}
 });
