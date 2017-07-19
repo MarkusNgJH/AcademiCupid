540979bcc02da1dc33b4ce5d73dd93d81b0c5902
Markus Ng <e0014936@u.nus.edu>
2017-07-19 20:36:03 +0800

    Group Schedule Fixed

--- AcademiCupid/client/TestTemplate.js
+++ AcademiCupid/client/TestTemplate.js
@@ -23,7 +23,7 @@ Template.TestTemplate.helpers({
 		}
 	},
 	getSchedules: function(){
-		var projectId = "DvxLbbj6aQjo5PKXt";
+		var projectId = FlowRouter.getParam('projectId');
 		var project = Projects.findOne(projectId);
 		var membersID = project.members;
 		var members = []
@@ -87,7 +87,7 @@ Template.TestTemplate.helpers({
 				numFree++;
 			}
 		}
-		var projectId = "DvxLbbj6aQjo5PKXt";
+		var projectId = FlowRouter.getParam('projectId');
 		var project = Projects.findOne(projectId);
 		var numMembers = project.numMembers;
 		var saturation = numFree/numMembers * 100;
